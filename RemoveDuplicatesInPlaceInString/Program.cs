a12f9e082be58af11dd393812af7ea5bb09e5f8d
gibeulah <gibeulah@microsoft.com>
2018-11-29 13:50:18 +0530

    First Commit

--- RemoveDuplicatesInPlaceInString/Program.cs
+++ RemoveDuplicatesInPlaceInString/Program.cs
@@ -6,7 +6,38 @@ namespace RemoveDuplicatesInPlaceInString
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Remove consecutive duplicates from string in place!");
+            Console.WriteLine("---------------------------------------------------");
+
+            Console.WriteLine("Enter the string containing duplicates");
+            String inputString = Console.ReadLine();
+            String resultString = GetDuplicatesRemovedString(inputString);
+            if (!string.IsNullOrEmpty(resultString))
+            {
+                Console.WriteLine();
+                Console.WriteLine("---The resultant string---");
+                Console.WriteLine(resultString);
+            }
+            else {
+                Console.WriteLine("Input and resultant strings are empty!");
+            }
+
+            Console.ReadLine();
+        }
+
+        private static String GetDuplicatesRemovedString(string inputString) {
+            if (string.IsNullOrEmpty(inputString)) {
+                return inputString;
+            }
+            Char[] charArray = new Char[inputString.Length];
+            int count = 0;
+            for (int i = 0; i < inputString.Length; i++) {
+                charArray[count++] = inputString[i];
+                while (i + 1 < inputString.Length && inputString[i] == inputString[i + 1]) {
+                    i++;
+                }
+            }
+            return new String(charArray);
         }
     }
 }
